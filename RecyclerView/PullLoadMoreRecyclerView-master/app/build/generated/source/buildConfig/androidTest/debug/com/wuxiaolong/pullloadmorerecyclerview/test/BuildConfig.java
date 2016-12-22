1d15df84e579382e1401205ddca05fdc4b1d8100
zyn9303 <m185138844>
2016-12-22 21:08:41 +0800

    1.添加RecyclerView

---
+++ RecyclerView/PullLoadMoreRecyclerView-master/app/build/generated/source/buildConfig/androidTest/debug/com/wuxiaolong/pullloadmorerecyclerview/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.wuxiaolong.pullloadmorerecyclerview.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.wuxiaolong.pullloadmorerecyclerview.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 112;
+  public static final String VERSION_NAME = "1.1.2";
+}
---
+++ RecyclerView/PullLoadMoreRecyclerView-master/app/build/generated/source/buildConfig/debug/com/wuxiaolong/pullloadmorerecyclerviewsample/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.wuxiaolong.pullloadmorerecyclerviewsample;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.wuxiaolong.pullloadmorerecyclerview";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 112;
+  public static final String VERSION_NAME = "1.1.2";
+}
